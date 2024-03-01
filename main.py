b90472f835ea9678697d2d1f8e4c3037b4f8b500
Md Abdur Sobhan Riad <93583569+riad5089@users.noreply.github.com>
2024-03-01 15:00:30 +0600

    Updated main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import json
 from PIL import Image
+import requests
+from io import BytesIO
 import streamlit as st
 import google.generativeai as gen_ai
+
 st.set_page_config(
-page_title="Image Captions",
-page_icon="📷",
-layout="centered",
+    page_title="Image Captions",
+    page_icon="📷",
+    layout="centered",
 )
 
 config_data = json.load(open("config.json"))
@@ -20,16 +23,27 @@ def get_image_caption(prompt, image, model=model):
     return image_caption
 
 st.title("📷 Snap Narrate")
-uploaded_image = st.file_uploader("Upload an image...", type=["jpg", "jpeg", "png"])  
 
+image_source = st.radio("Select Image Source:", ("Upload an image", "Enter Image URL"))
 
-if st.button("Generate Caption"):
-    image = Image.open(uploaded_image)
-    col1, col2 = st.columns(2)
-    with col1:
-        resized_img = image.resize((800, 500))
-        st.image(resized_img)
-    default_prompt = "write a short caption for this image"
-    caption = get_image_caption(default_prompt, image)
-    with col2:
-        st.info(caption)
+if image_source == "Upload an image":
+    uploaded_image = st.file_uploader("Upload an image...", type=["jpg", "jpeg", "png"])
+else:
+    image_url = st.text_input("Enter Image URL:")
+    if st.button("Generate Caption"):
+        try:
+            response = requests.get(image_url)
+            image = Image.open(BytesIO(response.content))
+        except Exception as e:
+            st.error(f"Error downloading the image from the provided URL: {str(e)}")
+            st.stop()
+            
+        col1, col2 = st.columns(2)
+        with col1:
+            resized_img = image.resize((800, 500))
+            st.image(resized_img)
+
+        default_prompt = "write a short caption for this image"
+        caption = get_image_caption(default_prompt, image)
+        with col2:
+            st.info(caption)
